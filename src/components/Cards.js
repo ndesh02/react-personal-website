478b45ce796a913ca4da2e765635afeb5f24f991
ndesh02 <54901274+ndesh02@users.noreply.github.com>
2021-03-09 19:34:02 -0500

    First commit

---
+++ src/components/Cards.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import CardItem from './CardItem';
+import './Cards.css';
+import image9 from './img-9.jpg';
+import image8 from './img-8.jpg';
+function Cards() {
+    return (
+        <div className='cards'>
+      <h1>Check out these EPIC Destinations!</h1>
+      <div className='cards__container'>
+        <div className='cards__wrapper'>
+          <ul className='cards__items' >
+            <CardItem
+              src={image8}
+              text='Explore the hidden waterfall deep inside the Amazon Jungle'
+              label='Adventure'
+              path='/services'
+            />
+            <CardItem
+              src={image9}
+              text='Travel through the Islands of Bali in a Private Cruise'
+              label='Luxury'
+              path='/services'
+            />
+          </ul>
+          <ul className='cards__items'>
+            <CardItem
+              src={image8}
+              text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
+              label='Mystery'
+              path='/services'
+            />
+            <CardItem
+              src={image9}
+              text='Experience Football on Top of the Himilayan Mountains'
+              label='Adventure'
+              path='/products'
+            />
+            
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Cards;
